d24ec99cbcc005e3ae2b6333ea3440e57763ae50
Kabilan <kabildeva8@gmail.com>
2025-10-08 17:00:01 +0530

    12th Mark

---
+++ 12th.py
@@ -0,0 +1,8 @@
+Tamil=int(input("Enter Your Tamil Mark:"))
+English=int(input("Enter Your English Mark:"))
+Maths=int(input("Enter Your Maths Mark:"))
+phycis=int(input("Enter Your phycis Mark:"))
+commputerscience=int(input("Enter Your commputerscience Mark:"))
+chemistry=int(input("Enter Your chemistry Mark:"))
+Total = Tamil + English + Maths + phycis + commputerscience + chemistry
+print("\n Your Mark Detail""\n",Tamil,"\n",English,"\n",Maths,"\n",chemistry,"\n",commputerscience,"\n",phycis,"\n",Total)
