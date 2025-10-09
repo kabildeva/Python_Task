389d0ad83676b1f7f4c92233763c5267b1e68d2b
Kabilan <kabildeva8@gmail.com>
2025-10-09 21:14:07 +0530

    Bank Deatils

---
+++ Bank.py
@@ -0,0 +1,6 @@
+Name=int(input("Enter Your Name Mark:"))
+Mail=int(input("Enter Your Mail Mark:"))
+Accounts=int(input("Enter Your Accounts Mark:"))
+Ph=int(input("Enter Your Ph Mark:"))
+Total = Name + Mail + Accounts + Ph
+print("\n Your Account Deatils","\n",Name,"\n",Mail,"\n",Accounts,"\n",Ph,"\n",Ph,"\n",Total)
